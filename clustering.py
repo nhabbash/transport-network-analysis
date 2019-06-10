ea5b8d0c319080442f497872fccd2d798059f1e0
dodicin <nostalgia.driver@gmail.com>
2019-06-10 16:40:48 +0200

    completed cell to cell flow graph, updated map

--- clustering.py
+++ clustering.py
@@ -1,7 +1,8 @@
-from geopy.distance import vincenty
+from geopy.distance import distance
 from scipy.spatial import Delaunay
 from collections import defaultdict
 import itertools
+import networkx as nx
 
 def add_member(cell, new_member, recompute_centroid = False):
     """
@@ -15,18 +16,20 @@ def add_member(cell, new_member, recompute_centroid = False):
         cell (dict): updated cell
     """
 
-    cell["members"].append(new_member)
+    cell["members"].append(new_member[0])
+
+    stop_coord = [float(i) for i in new_member[1]["coord"].split(",")]
+    cell["coords"].append(stop_coord)
 
     if recompute_centroid:
-        centroid = [0, 0]
-        for stop in cell["members"]:
-            stop_coord = [float(i) for i in stop[1]["coord"].split(",")]
-            centroid[0] += stop_coord[0]
-            centroid[1] += stop_coord[1]
+        centroid = [0.0, 0.0]
 
-        centroid = [x / len(cell["members"]) for x in centroid]
-        cell["coord"] = centroid
-    
+        for coord in cell["coords"]:
+            centroid[0] += coord[0]
+            centroid[1] += coord[1]
+
+        centroid = [x / len(cell["coords"]) for x in centroid]
+        cell["centroid"] = centroid
     return cell
 
 def find_closest(stop_coord, cells, radius):
@@ -42,15 +45,25 @@ def find_closest(stop_coord, cells, radius):
     """
 
     closest_cell_key = -1
+    min_dist = radius
+
+    # Candidate closest cell for stops that can't find any by the given radius
+    min_dist_candidate = 100
+    closest_cell_key_candidate = -1
     if cells:
         for key, cell in cells.items():
-            cell_coord = cell["coord"]
-            # Vincenty distance between two coordinates
-            dist = vincenty(stop_coord, cell_coord).km
-            if dist <= radius:
+            cell_centroid = cell["centroid"]
+            dist = distance(stop_coord, cell_centroid).km
+
+            if dist <= min_dist_candidate:
+                min_dist_candidate = dist
+                closest_cell_key_candidate = key
+
+            if dist <= min_dist:
+                min_dist = dist
                 closest_cell_key = key
 
-    return closest_cell_key
+    return closest_cell_key, closest_cell_key_candidate
      
 def cluster_stops(stops, radius):
     """
@@ -69,35 +82,81 @@ def cluster_stops(stops, radius):
     # Generate cells
     for stop in stops:
         stop_coord = [float(i) for i in stop[1]["coord"].split(",")]
-        key = find_closest(stop_coord, cells, radius)
+        key, _ = find_closest(stop_coord, cells, radius)
 
         if key != -1:
-            key = find_closest(stop_coord, cells, radius)
             cells[key] = add_member(cells[key], stop, True)
         else:
-            cell = {"coord" : stop_coord, "members" : [stop]}
-            cells[len(cells)] = cell  
+            cell = {"centroid" : stop_coord, "members" : [stop[0]], "coords" : [stop_coord]}
+            cells[len(cells)] = cell
 
     # Remove members while keeping centroids
-    for key, cell in cells.items():
+    for _, cell in cells.items():
+        cell["centroid_generators"] = cell["members"]
+        cell["centroid_coords"] = cell["coords"]
         cell["members"] = []
+        cell["coords"] = []
 
     # Redistribute stops
+    stops_mapping = {}
     for stop in stops:
         stop_coord = [float(i) for i in stop[1]["coord"].split(",")]
-        key = find_closest(stop_coord, cells, radius)
-        cells[key] = add_member(cells[key], stop)
-    
-    return cells
+        key, secondary_key = find_closest(stop_coord, cells, radius)
 
-def get_neighbor_list(points):
+        if key == -1:
+            key = secondary_key
+
+        cells[key] = add_member(cells[key], stop)
+        stops_mapping[stop[0]] = key
+
+    return cells, stops_mapping
+
+def get_neighbor_list(points, radius = 5):
+    """
+    Generates neighbor list for each node through the Delaunay triangulation
+
+    Args:
+        points (list): List of coordinates
+        radius (float): Maximum distance between neighbors
+
+    Returns:
+        neighbors (dict): a dict of neighbors indices (based on points indices)
+    """
+
     tri = Delaunay(points)
     neighbors = defaultdict(set)
-    for p in tri.vertices:
-        for i,j in itertools.combinations(p,2):
-            neighbors[i].add(j)
-            neighbors[j].add(i)
+    
+    for p in tri.vertices:        
+        for i, j in itertools.combinations(p, 2):
+            dist = distance(points[i], points[j]).km
+            if dist <= radius:
+                neighbors[i].add(j)
+                neighbors[j].add(i)
+
     return neighbors
 
-#def c2c_flow(graph, ):
+def c2c_flow_graph(net, stops_mapping):
+    """
+    Generates a region graph with cell-to-cell flow between the subnodes in the regions
 
+    Args:
+        net (networkx graph): Complete network containing all the edges
+        stops_mapping (dict): Mapping of stops and their corrispective region
+
+    Returns:
+        c2c_graph (networkx graph)
+    """
+
+    g = nx.Graph()
+    g.add_nodes_from(list(set(stops_mapping.values())))
+    
+    for stop, cur_region in stops_mapping.items():
+        edges = net.edges(stop)
+        for edge in edges:
+            dest_region = stops_mapping[edge[1]]
+            if dest_region != cur_region:
+                if g.has_edge(cur_region, dest_region):
+                    g[cur_region][dest_region]["weight"] += 1
+                else:
+                    g.add_edge(cur_region, dest_region, weight = 1)
+    return g
--- merge_graphs.py
+++ merge_graphs.py
@@ -7,21 +7,30 @@ import matplotlib.pyplot as plt
 pp = pprint.PrettyPrinter(indent = 4)
 
 metro = nx.read_gml("data/graph/metro.gml")
-bus = nx.read_gml("data/graph/bus.gml").to_undirected()
-tram = nx.read_gml("data/graph/tram.gml").to_undirected()
+bus = nx.Graph(nx.read_gml("data/graph/bus.gml").to_undirected())
+tram = nx.Graph(nx.read_gml("data/graph/tram.gml").to_undirected())
 
 metro_nodes = list(metro.nodes(data = True))
-bus_nodes = list(bus.nodes(data = True))
-tram_nodes = list(tram.nodes(data = True))
 
-stops = metro_nodes# + bus_nodes + tram_nodes
+# Bus and tram share some stops, some processing is needed before uniting the nets. In the composition, bus attributes take the precedence
+bus_tram = nx.compose(tram, bus)
 
-regions = cl.cluster_stops(stops, 1)
-points = [x["coord"] for _, x in regions.items()]
+for node in tram.nodes(data = True):
+    if node[1]["routes"] != bus_tram.node[node[0]]["routes"]:
+        bus_tram.node[node[0]]["routes"] += "," + (node[1]["routes"])
 
-region_neighbors = cl.get_neighbor_list(points)
+net = nx.union(metro, bus_tram)
+
+stops = list(net.nodes(data = True))
+
+regions, stops_mapping = cl.cluster_stops(stops, 1)
+points = [x["centroid"] for _, x in regions.items()]
+
+region_neighbors = cl.get_neighbor_list(points, 4.5)
 neighbor_graph = nx.Graph(region_neighbors)
 
-for n in neighbor_graph:
-    neighbor_graph.nodes[n]["coord"] = regions[n]["coord"]
+for n in neighbor_graph:   
+    neighbor_graph.nodes[n]["centroid"] = regions[n]["centroid"]
+    neighbor_graph.nodes[n]["stops"] = [member for member in regions[n]["members"]]
 
+c2c_graph = cl.c2c_flow_graph(net, stops_mapping)
